f6acafc1c5ad51e8459a81d5a7ec2d734922bb25
Dan0Silva <danilo@buildbox.com.br>
2024-04-16 01:55:04 -0300

    creating post list

---
+++ public/_posts/2024-04-02-bem-vindos.md
@@ -0,0 +1,19 @@
+---
+layout: post
+title:  "Bem-vindos ao Café Aleatório!"
+date:   2024-04-02 13:53:08 -0300
+categories: geral
+---
+Olá a todos e sejam muito bem-vindos ao Café Aleatório, o novo espaço digital que estamos criando com muito entusiasmo. Aqui, eu, Edwin, junto com alguns amigos, estamos embarcando em uma jornada emocionante para compartilhar nossas paixões, interesses e aprendizados com vocês, nossos leitores.
+
+Como engenheiro de software em formação, sempre fui fascinado pelo poder da tecnologia e pela maneira como ela molda nosso mundo. Mas, assim como um bom café, a vida é uma mistura de sabores e aromas diversos. Por isso, este blog não se limitará apenas a discussões sobre códigos e algoritmos (embora isso certamente tenha seu espaço aqui!), mas também abordará uma variedade de temas, desde ciência e arte até viagens e gastronomia.
+
+A ideia por trás do Café Aleatório é simples: queremos criar um espaço colaborativo onde possamos compartilhar nossas experiências, opiniões e descobertas com vocês, nossos leitores queridos. Todos nós temos nossas próprias perspectivas únicas e é essa diversidade que enriquece nossas conversas e reflexões.
+
+Então, o que você pode esperar encontrar aqui? Bem, um pouco de tudo! Desde resenhas de livros e filmes até tutoriais de programação, passando por relatos de viagens e dicas culinárias, estaremos explorando os mais diversos assuntos de forma descontraída e autêntica.
+
+Além disso, estamos muito abertos à participação de vocês, nossos leitores! Queremos ouvir suas histórias, suas sugestões de temas e suas opiniões. Este é um espaço de diálogo e estamos ansiosos para construí-lo juntos.
+
+Então, pegue sua xícara de café (ou chá, se preferir!) e vamos nessa!
+
+Um abraço do seu amigo do bairro, TheCodingCat.
--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -1,16 +1,48 @@
 import React from "react";
+import postlist from '../../../public/postlist.json';
 
 export default () => {
+  const renderList = () => {
+    const newList = postlist.names.map((element, index) => {
+      const i = index.toString();
+      console.log(`ola: [${i.padStart(3, '&nbsp')}]`)
+
+      return (
+        <div className="mb-4">
+          <span className="text-2xl font-medium text-sky-700 mr-1">{i.padStart(3, ' ')}. </span>
+          <button className="text-2xl font-medium text-sky-700 underline">{element}</button>
+        </div>
+      )
+    })
+
+    return newList
+  }
+
   return (
-    <div className="flex-grow flex flex-row bg-zinc-400 mx-28 my-12 rounded-xl 
+    <div className="flex-grow flex flex-row bg-zinc-400 max-h-[82svh] mx-28 my-12 rounded-xl 
       overflow-hidden shadow-md max-w-screen-lg">
       <div className="w-24 bg-zinc-900 pt-16">
         <div className="w-14 h-5 bg-zinc-400 z-10 absolute rounded-xl ml-[-22px]"/>    
         <div className="w-14 h-5 bg-zinc-400 z-10 absolute rounded-xl mt-[40px] ml-[-22px]"/> 
-        <div className="w-14 h-5 bg-zinc-400 z-10 absolute rounded-xl mt-[80px] ml-[-22px]"/>
+        <div className="w-14 h-5 bg-zinc-400 z-10 absolute rounded-xl mt-[80px] ml-[-22px]"/> 
       </div>
+      
       <div className="flex-grow flex-col bg-zinc-50 shadow-inner px-6 py-7">
-        <span className="font-nsmono text-3xl font-semibold">Posts</span>
+        <span className="font-nsmono text-4xl font-semibold">Posts</span>
+        <div className="h-1 w-full bg-zinc-700 my-3 rounded-full"/>        
+        
+        <div className="px-3 py-6 h-[90%] overflow-y-auto">
+          {renderList()}
+
+          <style jsx="true">
+            {`
+            ::-webkit-scrollbar-thumb {
+              background: transparent;
+            }
+            `}
+          </style>
+        </div>
+        
         <div className="h-1 w-full bg-zinc-700 mt-3 rounded-full"/>
       </div>
     </div>
